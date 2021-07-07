fde67d9a1085a4e0ffd31e557c042963a25da198
youheihosoda <youhe0122@gmail.com>
2021-07-07 07:15:24 +0000

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,12 +24,7 @@
 //= require turbolinks
 //= require_tree .
 
-// $(document).on('turbolinks:load', function() {
-//   $('.jscroll').jscroll({
-//     nextSelector: 'a[rel=next].page-link', //次に表示させたいページのリンク先
-//     contentSelector: '.jscroll' //[jscroll]に追加表示させる
-//   });
-// });
+
 
 
 
